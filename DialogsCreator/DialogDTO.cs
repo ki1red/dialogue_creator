72a4930f2a314fa51c1dae1aac272cfcb6f82676
ki1red <44669070+ki1red@users.noreply.github.com>
2023-01-22 20:30:26 +0700

    begin

---
+++ DialogsCreator/DialogDTO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogsCreator
+{
+    [Serializable]
+    public class DialogDTO
+    {
+        public DialogLineDTO[] dialogLines;
+        public TextPath[] TextPaths;
+    }
+
+    [Serializable]
+    public class DialogLineDTO
+    {
+        public int id;
+        public int textId;
+        public OptionDTO[] options;
+        public RequiredAnswerDTO[] requiredAnswers;
+        public int[] nextLineId;
+        public string nextDialogPath;
+    }
+
+    [Serializable]
+    public class OptionDTO
+    {
+        public int id;
+        public int textId;
+        public int[] nextLineId;
+        public string nextDialogPath;
+        public RequiredAnswerDTO[] requiredAnswers;
+    }
+
+    [Serializable]
+    public class RequiredAnswerDTO
+    {
+        public int questionId;
+        public int answerId;
+    }
+
+    [Serializable]
+    public class TextPath
+    {
+        public string localization;
+        public string path;
+    }
+}
---
+++ DialogsCreator/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.Json;
+
+namespace DialogsCreator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string pathFile;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            pathFile = Environment.CurrentDirectory;
+            
+        }
+
+        private void CreateTable(string pathToFile)
+        {
+            
+        }
+
+        private void EnableAddingDialogues()
+        {
+
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            string path = pathFile;
+
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.dlag)|*.dlag|All files (*.*)|*.*"; // фильтры файлов
+            openFileDialog.InitialDirectory = @path; // путь по умолчанию для окна
+            if (openFileDialog.ShowDialog() == true)
+            {
+                path = openFileDialog.FileName;
+                pathFile = path;
+            }
+            else
+                return;
+
+            CreateTable(path);
+        }
+
+        private void CreateFile_Click(object sender, RoutedEventArgs e)
+        {
+            string path = pathFile;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.dlag)|*.dlag|All files (*.*)|*.*";
+            saveFileDialog.InitialDirectory = @path;
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                path = saveFileDialog.FileName;
+                Stream myStream = saveFileDialog.OpenFile();
+
+                pathFile = path;
+            }
+            else
+                return;
+
+            CreateTable(path);
+        }
+
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            Stream myStream;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = pathFile;
+            myStream = saveFileDialog.OpenFile();
+        }
+    }
+}

b21d298f8f73c66347bc13888c367e661adddfe8
ki1red <44669070+ki1red@users.noreply.github.com>
2023-02-05 21:38:27 +0700

    v1.9.3
    Added the ExportWindow

---
+++ DialogsCreator/EncryptorJSON.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Input;
+using Newtonsoft.Json;
+
+namespace DialogsCreator
+{
+    public class EncryptJSON
+    {
+        public static void ain()
+        {
+            //var data = new { name = "John Doe", age = 35 };
+            var key = new byte[32] { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32 }; // Key should be 32 bytes long
+            string data = "{\r\n  \"language\": \"ru\",\r\n  \"positionCanvas\": \"10010,50013\",\r\n  \"elements\": [\r\n    {\r\n      \"idElement\": 1,\r\n      \"pathToSound\": null,\r\n      \"pathToImage\": null,\r\n      \"author\": \"1\",\r\n      \"question\": {\r\n        \"idElement\": 1,\r\n        \"text\": \"2\",\r\n        \"type\": 1,\r\n        \"nextElement\": {\r\n          \"idElement\": -1,\r\n          \"type\": 0,\r\n          \"textElement\": null\r\n        },\r\n        \"requests\": []\r\n      },\r\n      \"answers\": [\r\n        {\r\n          \"idElement\": 1,\r\n          \"text\": \"3\",\r\n          \"type\": 2,\r\n          \"nextElement\": {\r\n            \"idElement\": 2,\r\n            \"type\": 1,\r\n            \"textElement\": \"22\"\r\n          },\r\n          \"requests\": []\r\n        },\r\n        {\r\n          \"idElement\": 1,\r\n          \"text\": \"4\",\r\n          \"type\": 2,\r\n          \"nextElement\": {\r\n            \"idElement\": 3,\r\n            \"type\": 1,\r\n            \"textElement\": \"222\"\r\n          },\r\n          \"requests\": []\r\n        }\r\n      ],\r\n      \"point\": \"10237,50163\"\r\n    },\r\n    {\r\n      \"idElement\": 2,\r\n      \"pathToSound\": null,\r\n      \"pathToImage\": null,\r\n      \"author\": \"11\",\r\n      \"question\": {\r\n        \"idElement\": 2,\r\n        \"text\": \"22\",\r\n        \"type\": 1,\r\n        \"nextElement\": {\r\n          \"idElement\": -1,\r\n          \"type\": 0,\r\n          \"textElement\": null\r\n        },\r\n        \"requests\": [\r\n          {\r\n            \"idElement\": 1,\r\n            \"type\": 2,\r\n            \"textElement\": \"3\"\r\n          }\r\n        ]\r\n      },\r\n      \"answers\": [\r\n        {\r\n          \"idElement\": 2,\r\n          \"text\": \"33\",\r\n          \"type\": 2,\r\n          \"nextElement\": {\r\n            \"idElement\": -1,\r\n            \"type\": 0,\r\n            \"textElement\": null\r\n          },\r\n          \"requests\": []\r\n        }\r\n      ],\r\n      \"point\": \"11225,50138\"\r\n    },\r\n    {\r\n      \"idElement\": 3,\r\n      \"pathToSound\": null,\r\n      \"pathToImage\": null,\r\n      \"author\": \"111\",\r\n      \"question\": {\r\n        \"idElement\": 3,\r\n        \"text\": \"222\",\r\n        \"type\": 1,\r\n        \"nextElement\": {\r\n          \"idElement\": -1,\r\n          \"type\": 0,\r\n          \"textElement\": null\r\n        },\r\n        \"requests\": [\r\n          {\r\n            \"idElement\": 1,\r\n            \"type\": 2,\r\n            \"textElement\": \"4\"\r\n          }\r\n        ]\r\n      },\r\n      \"answers\": [\r\n        {\r\n          \"idElement\": 3,\r\n          \"text\": \"333\",\r\n          \"type\": 2,\r\n          \"nextElement\": {\r\n            \"idElement\": -1,\r\n            \"type\": 0,\r\n            \"textElement\": null\r\n          },\r\n          \"requests\": []\r\n        },\r\n        {\r\n          \"idElement\": 3,\r\n          \"text\": \"444\",\r\n          \"type\": 2,\r\n          \"nextElement\": {\r\n            \"idElement\": -1,\r\n            \"type\": 0,\r\n            \"textElement\": null\r\n          },\r\n          \"requests\": []\r\n        }\r\n      ],\r\n      \"point\": \"11048,50590\"\r\n    }\r\n  ],\r\n  \"linkeds\": [\r\n    {\r\n      \"OutIdDialogView\": 1,\r\n      \"OutIdOptionView\": 0,\r\n      \"OutIdBindingView\": 1,\r\n      \"InIdDialogView\": 2,\r\n      \"InIdOptionView\": -1,\r\n      \"InIdBindingView\": 0,\r\n      \"LinesCoords\": [\r\n        {\r\n          \"X1\": 10652.0,\r\n          \"X2\": 11210.0,\r\n          \"Y1\": 50263.30333333334,\r\n          \"Y2\": 50162.08666666667\r\n        }\r\n      ]\r\n    },\r\n    {\r\n      \"OutIdDialogView\": 1,\r\n      \"OutIdOptionView\": 1,\r\n      \"OutIdBindingView\": 1,\r\n      \"InIdDialogView\": 3,\r\n      \"InIdOptionView\": -1,\r\n      \"InIdBindingView\": 0,\r\n      \"LinesCoords\": [\r\n        {\r\n          \"X1\": 10652.0,\r\n          \"X2\": 11033.0,\r\n          \"Y1\": 50318.433333333334,\r\n          \"Y2\": 50614.08666666667\r\n        }\r\n      ]\r\n    }\r\n  ]\r\n}";
+            // Serialize JSON to string
+            var json = JsonConvert.SerializeObject(data);
+
+            // Encrypt JSON string
+            var encryptedJson = Encrypt(json, key);
+
+            // Write encrypted JSON to file
+            File.WriteAllBytes("encrypted.json", encryptedJson);
+
+            // Decrypt JSON string
+            var decryptedJson = Decrypt(encryptedJson, key);
+
+            // Deserialize JSON from string
+            var decryptedData = JsonConvert.DeserializeObject<string>(decryptedJson);
+
+            //Console.WriteLine(decryptedData.name);
+            //Console.WriteLine(decryptedData.age);
+        }
+
+        static byte[] Encrypt(string plainText, byte[] key)
+        {
+            using var aes = Aes.Create();
+            aes.Key = key;
+            aes.GenerateIV();
+
+            using var encryptor = aes.CreateEncryptor();
+            using var ms = new MemoryStream();
+            using var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+            using var sw = new StreamWriter(cs);
+            sw.Write(plainText);
+            sw.Flush();
+            cs.FlushFinalBlock();
+
+            var encrypted = ms.ToArray();
+            var result = new byte[aes.IV.Length + encrypted.Length];
+            aes.IV.CopyTo(result, 0);
+            encrypted.CopyTo(result, aes.IV.Length);
+            return result;
+        }
+
+        static string Decrypt(byte[] cipherText, byte[] key)
+        {
+            using var aes = Aes.Create();
+            aes.Key = key;
+            var iv = new byte[aes.BlockSize / 8];
+            var encrypted = new byte[cipherText.Length - iv.Length];
+            Array.Copy(cipherText, 0, iv, 0, iv.Length);
+            Array.Copy(cipherText, iv.Length, encrypted, 0, encrypted.Length);
+            aes.IV = iv;
+
+            using var decryptor = aes.CreateDecryptor();
+            using var ms = new MemoryStream(encrypted);
+            using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+            using var sr = new StreamReader(cs);
+            return sr.ReadToEnd();
+        }
+    }
+}
---
+++ DialogsCreator/ExportWindow.xaml.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DialogsCreator
+{
+    public partial class ExportWindow : Window
+    {
+        private DialogDFD dialog;
+        private Dictionary<int, string> elements;
+        public ExportWindow(string pathToDfdFile, string nameDfdFile, string formatDfdFile, DialogDFD dialog)
+        {
+            InitializeComponent();
+
+            this.dialog = dialog;
+            InitializeElements();
+
+            InitializeDirectoryes(pathToDfdFile);
+            InitializeGroupFileDfd(nameDfdFile, formatDfdFile);
+            InitializeGroupFileDlag();
+            InitializeGroupFileDlt();
+            InitializeGroupFileDlv();
+            InitializeGroupStartDialog();
+            InitializeGroupEndDialog();
+            InitializeNextFileDlag();
+        }
+
+        private void InitializeDirectoryes(string initialDirectory)
+        {
+            FileDlag.InitialDirectory = initialDirectory;
+            FileDlt.InitialDirectory = initialDirectory;
+            FileDlv.InitialDirectory = initialDirectory;
+        }
+        private void InitializeGroupFileDfd(string nameFile, string formatFile)
+        {
+            this.TextBox_devFile.Text = $"{nameFile}.{formatFile}";
+            this.TextBox_devFile.IsEnabled = false;
+
+            this.CheckBox_addDevFile.IsChecked = true;
+            this.CheckBox_addDevFile.IsEnabled = false;
+        }
+        private void InitializeGroupFileDlag()
+        {
+            this.TextBox_gameFile.Text = null;
+            this.TextBox_gameFile.IsEnabled = false;
+
+            this.Button_createGameFile.IsEnabled = true;
+            this.Button_createGameFile.Click += Button_createGameFile_Click;
+        }
+        private void InitializeGroupFileDlt()
+        {
+            this.TextBox_gameFile.Text = null;
+            this.TextBox_gameFile.IsEnabled = false;
+
+            this.Button_createFileTranslate.IsEnabled = true;
+            this.Button_createFileTranslate.Click += Button_createFileTranslate_Click;
+        }
+        private void InitializeGroupFileDlv()
+        {
+            this.TextBox_gameFile.Text = null;
+            this.TextBox_gameFile.IsEnabled = true;
+
+            this.CheckBox_addTrashFile.IsEnabled = true;
+            this.CheckBox_addTrashFile.Click += CheckBox_addTrashFile_Click;
+        }
+        private void InitializeGroupStartDialog()
+        {
+            this.ComboBox_startDialog.IsEnabled = true;
+            for (int i = 0; i < dialog.elements.Length; i++)
+            {
+                bool check = true;
+
+                ElementDFD element = dialog.elements[i];
+                if (element.question.requests.Length != 0)
+                {
+                    check = false;
+                    continue;
+                }
+                for (int j = 0; j < element.answers.Length; j++)
+                {
+                    SayingElementDFD saying = element.answers[j];
+
+                    if (saying.requests.Length != 0)
+                    {
+                        check = false;
+                        break;
+                    }
+
+                }
+
+                if (check)
+                    this.ComboBox_startDialog.Items.Add(element.question.text);
+            }
+            if (this.ComboBox_startDialog.Items.Count == 0)
+            {
+                MessageBox.Show("Нет ни одного начального элемента", "Ошибка");
+                return; // TODO исключение
+            }
+            else if (this.ComboBox_startDialog.Items.Count > 1)
+            {
+                MessageBox.Show("Слишком много начальных элементов", "Ошибка");
+            }
+
+            this.ComboBox_startDialog.SelectedIndex = 0;
+
+            this.CheckBox_addStartDialog.IsEnabled = true;
+            this.CheckBox_addStartDialog.Click += CheckBox_addStartDialog_Click;
+        }
+        private void InitializeGroupEndDialog()
+        {
+            this.ComboBox_endDialog.IsEnabled = true;
+            for (int i = 0; i < dialog.elements.Length; i++)
+            {
+                bool check = true;
+
+                ElementDFD element = dialog.elements[i];
+                SayingElementDFD test = new SayingElementDFD();
+                test.requests = null;
+
+                if (!sr(element.question.nextElement))
+                {
+                    check = false;
+                    continue;
+                }
+                for (int j = 0; j < element.answers.Length; j++)
+                {
+                    SayingElementDFD saying = element.answers[j];
+
+                    if (!sr(saying.nextElement))
+                    {
+                        check = false;
+                        break;
+                    }
+
+                }
+
+                if (check)
+                    this.ComboBox_endDialog.Items.Add(element.question.text);
+            }
+            if (this.ComboBox_endDialog.Items.Count == 0)
+            {
+                MessageBox.Show("Нет ни одного конечного элемента", "Ошибка");
+                return; // TODO исключение
+            }
+            else if (this.ComboBox_endDialog.Items.Count > 1)
+            {
+                MessageBox.Show("Слишком много конечных элементов", "Ошибка");
+            }
+
+            this.ComboBox_endDialog.SelectedIndex = 0;
+
+            this.CheckBox_addEndDialog.IsEnabled = true;
+            this.CheckBox_addEndDialog.Click += CheckBox_addEndDialog_Click;
+        }
+        private void InitializeNextFileDlag()
+        {
+            this.TextBox_selectNextFile.Text = null;
+            this.TextBox_selectNextFile.IsEnabled = false;
+
+            this.Button_selectNextFile.IsEnabled = true;
+            this.Button_selectNextFile.Click += Button_selectNextFile_Click;
+        }
+        private void InitializeElements()
+        {
+            this.elements = new Dictionary<int, string>(this.dialog.elements.Length);
+
+            for (int i = 0; i < this.elements.Count; i++)
+            {
+                this.elements[i] = this.dialog.elements[i].question.text;
+            }
+        }
+
+        private void Button_createGameFile_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        private void Button_createFileTranslate_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        private void CheckBox_addTrashFile_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        private void CheckBox_addStartDialog_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        private void CheckBox_addEndDialog_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        private void Button_selectNextFile_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool sr(SayingElementDFD first)
+        {
+            if (first.text == null && first.type == TypeSayingElementDFD.none && first.idElement == -1 && (first.requests == null || first.requests.Length == 0))
+                return true;
+            else
+                return false;
+        }
+    }
+}
